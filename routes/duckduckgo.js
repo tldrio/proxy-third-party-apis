19a8873b67c11e22f2efc4bcaab403b2f9d8815e
Charles Miglietti <charles.miglietti@yahoo.fr>
2013-04-25 12:30:38 +0200

    pause for now

--- routes/duckduckgo.js
+++ routes/duckduckgo.js
@@ -11,35 +11,37 @@ module.exports = function (req, res, next) {
   async.map( req.body.batch
    , function (entry, callback) {
       console.log('[REQUEST]', entry);
-       request.get({ url: 'https://api.duckduckgo.com/?q=' + entry.replace(' ','+') +'&o=json'
-                   , timeout: 500
-                  }, function (err, res, body) {
-                    if (err) {
-                      return callback(err);
-                    }
+      callback();
+       //request.get({ url: 'https://api.duckduckgo.com/?q=' + entry.replace(' ','+') +'&o=json'
+                   //, timeout: 500
+                  //}, function (err, res, body) {
+                    //if (err) {
+                      //return callback(err);
+                    //}
 
-                    try {
-                      var response = JSON.parse(body)
-                        , previewText = response.Abstract || response.Definition;
+                    //try {
+                      //var response = JSON.parse(body)
+                        //, previewText = response.Abstract || response.Definition;
 
-                      if (previewText.length) {
-                        callback(null, { entry: entry, previewText: previewText, heading: response.Heading });
-                      } else {
-                        callback(null, {});
-                      }
-                    } catch(e) {
-                      return callback(e);
-                    }
-                  });
+                      //if (previewText.length) {
+                        //callback(null, { entry: entry, previewText: previewText, heading: response.Heading });
+                      //} else {
+                        //callback(null, {});
+                      //}
+                    //} catch(e) {
+                      //return callback(e);
+                    //}
+                  //});
 
    }, function (err, results) {
         if (err) {
           console.log('[ERROR]', err);
           return res.send(408, err);
         }
-        results = _.filter(results, function(result) { return result.previewText;});
-        console.log('[RESULTS]', results);
-        res.json(200, results);
+        //results = _.filter(results, function(result) { return result.previewText;});
+        //console.log('[RESULTS]', results);
+        //res.json(200, results);
+      res.json(200, []);
   });
 
 }
