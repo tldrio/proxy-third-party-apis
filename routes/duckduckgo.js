e6936064ec0cb91718227aa8014efc664da43f77
Charles Miglietti <charles.miglietti@yahoo.fr>
2013-04-25 14:16:49 +0200

    cache responses from ddg

--- routes/duckduckgo.js
+++ routes/duckduckgo.js
@@ -1,6 +1,8 @@
 var async = require('async')
   , request = require('request')
-  , _ = require('underscore');
+  , _ = require('underscore')
+  , client = require('redis').createClient()
+  , prefix = 'preview-ddg:';
 
 module.exports = function (req, res, next) {
    var batch = []
@@ -11,21 +13,35 @@ module.exports = function (req, res, next) {
   async.map( req.body.batch
    , function (entry, callback) {
       console.log('[REQUEST]', entry, req.ip);
-      callback();
-       //request.get({ url: 'https://api.duckduckgo.com/?q=' + entry.replace(' ','+') +'&o=json'
-                   //, timeout: 500
+      return callback();
+      //client.get(prefix+ entry, function (err, reply) {
+        //if (err) {
+          //return callback(err);
+        //}
+        //if (reply) {
+          //console.log('[CACHE]', entry);
+          //callback(null, JSON.parse(reply));
+        //}
+        //else {
+         //console.log('[ASK DDG]', entry);
+         //request.get({ url: 'https://api.duckduckgo.com/?q=' + entry.replace(' ','+') +'&o=json'
+                   //, timeout: 1000
                   //}, function (err, res, body) {
                     //if (err) {
+                        //console.log('[ERR] On single request',err);
                       //return callback(err);
                     //}
 
                     //try {
                       //var response = JSON.parse(body)
                         //, previewText = response.Abstract || response.Definition;
+                        //console.log('[RESPONSE]', entry, previewText);
 
                       //if (previewText.length) {
+                        //client.set(prefix+ entry, JSON.stringify({ entry: entry, previewText: previewText, heading: response.Heading }));
                         //callback(null, { entry: entry, previewText: previewText, heading: response.Heading });
                       //} else {
+                        //client.set(prefix+ entry, JSON.stringify({}));
                         //callback(null, {});
                       //}
                     //} catch(e) {
@@ -33,9 +49,13 @@ module.exports = function (req, res, next) {
                     //}
                   //});
 
+        //}
+
+      //});
+
    }, function (err, results) {
         if (err) {
-          console.log('[ERROR]', err);
+          console.log('[ERROR] in batch', err, results);
           return res.send(408, err);
         }
         //results = _.filter(results, function(result) { return result.previewText;});
